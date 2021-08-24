cfb490106891540a988694d1586a5a4539b536c8
Janiru <janiru160@gmail.com>
2021-08-24 16:16:33 +0530

    connected with the movie database

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import React from 'react';
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
       <h1>netflix</h1>      
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
 
-export default App;
+export default App
--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,4 @@
-const APIKEY = "59a92d3e16772a0a5ad32ce003daf4c7";
+const API_KEY = "59a92d3e16772a0a5ad32ce003daf4c7";
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
