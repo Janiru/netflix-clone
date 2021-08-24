6bbcd8fdfce430a14c66955cf685220f38ff2f6f
Janiru <janiru160@gmail.com>
2021-08-24 17:31:20 +0530

    displaying thumbnails

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import axios from './axios'
+import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
@@ -22,7 +23,9 @@ function Row({title, fetchUrl}) {
 
             <div className="row_posters">
                 {movies.map(movie => (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                    <img
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
                 ))}
             </div>
 
