d78d2ec51f282f0f1ff0d8d1f3a60926036aff82
Janiru <janiru160@gmail.com>
2021-08-24 16:54:51 +0530

    displaying data on the website

--- src/Row.js
+++ src/Row.js
@@ -1,20 +1,31 @@
 import React, { useState,useEffect } from 'react';
 import axios from './axios'
 
+const base_url = "https://image.tmdb.org/t/p/original/"
+
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
     useEffect(() => {
         async function fetchData(){
             const request = await axios.get(fetchUrl);
-            console.log(request);
+            setMovies(request.data.results);
             return request;
         }
         fetchData();
-    }, [])
+    }, [fetchUrl]);
+
+    console.table(movies)
 
     return(
-        <div>
+        <div className="row">
             <h2>{title}</h2>
+
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                ))}
+            </div>
+
         </div>
     )
 }
